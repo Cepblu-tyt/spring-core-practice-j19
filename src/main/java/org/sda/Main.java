938cbca8efc2c95fcb225f03742b4dd444210b26
Cepblu-tyt <sergei.oksanen@gmail.com>
2023-02-20 19:07:04 +0200

    1

---
+++ src/main/java/org/sda/Main.java
@@ -0,0 +1,12 @@
+package org.sda;
+
+/**
+ *
+ * @author Sergei Oksanen
+ * @date ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
